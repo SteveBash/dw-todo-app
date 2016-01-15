724b1ac49f5bcc03eb28fbaae96bccc1c46da5fa
SteveBash <ioexception02@gmail.com>
2016-01-15 01:53:56 -0500

    Add jackson example

--- Example.java
+++ Example.java
@@ -1,16 +1,22 @@
 
 import models.Todo;
+
 import com.fasterxml.jackson.databind.*;
+import java.io.*;
 
 class Example{
     public static void main(String[] args){
+        ObjectMapper om = new ObjectMapper();
         Todo t1 = new Todo();
         Todo t2 = new Todo();
-        t1.name = "Buy bread";
-        t1.done= false;
-        t2.name = "Learn webdev";
-        t2.done= true;
+        t2.name = "Watch MTV";
+        t2.done = true;
+        try {
+            t1 = om.readValue(new File("resources/todo1.json"), Todo.class);
+            om.writeValue(new File("resources/todo2.json"), t2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println(t1);
-        System.out.println(t2);
     }
 }
