8c94067cb31c2bdf886fe9a3bfafc45151119c27
SteveBash <ioexception02@gmail.com>
2016-01-15 19:19:42 -0500

    Add json servlet example

--- src/main/java/ExampleServlet.java
+++ src/main/java/ExampleServlet.java
@@ -1,16 +1,24 @@
 
+import models.Todo;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import org.eclipse.jetty.http.*;
+import com.fasterxml.jackson.databind.*;
+import java.io.*;
 
 public class ExampleServlet extends HttpServlet{
     
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res){
-        res.setContentType("text/html"); 
-        res.setStatus(HttpStatus.OK_200);
+        ObjectMapper om = new ObjectMapper();
+        Todo t1 = new Todo();
+        res.setContentType("application/json"); 
         try {
-            res.getWriter().println("<h1>Hello Java Servlet</h1>");
+            PrintWriter writer = res.getWriter();
+            t1 = om.readValue(new File("src/main/resources/todo1.json"), Todo.class);
+            res.setStatus(HttpStatus.OK_200);
+            om.writeValue(writer, t1);
         } catch (Exception e) {
             e.printStackTrace();
         }
